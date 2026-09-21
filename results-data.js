70591dcdc0ec38691e531cdb0c72fcf8a18f4a5d
Francesco Alderighi <f.alderighi@sesa.it>
2026-09-21 10:12:52 +0200

    Refresh results module cache

--- results-data.js
+++ results-data.js
@@ -1,5 +1,5 @@
 import './auth.js';
-import { initResults } from './results.js';
+import { initResults } from './results.js?v=20260921-match-detail';
 import { selectFeaturedMatches, isCapraiaMatch } from './data/calendar-logic.js';
 
 const root = document.querySelector('#partite');
